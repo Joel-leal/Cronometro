25775c9d985a2c94fc5ccf4dcdfd6079ef66995c
Joel-leal <joel.lasi@hotmail.com>
2021-04-11 23:28:05 -0300

    cronometro atualizado

--- funcCronometro.js
+++ funcCronometro.js
@@ -1,16 +1,41 @@
+let red = 0;
+let green = 255;
+let blue = 0;
+let alpha = 1;
+let contador = document.getElementById("time").value;
 
-    let contador = document.getElementById("time").value
-    function timeTranslator(){
-    let pegaElemento = parseInt(document.getElementById("time").value) + 1
-    contador = pegaElemento;
-    setInterval(myFunction, 1000)
-    }
-    function myFunction(){
-        if(contador > 0){
-        contador -= 1;
-        const element = document.getElementById("retCronometro")
-        element.innerText = contador 
-        }
-    }
+function timeTranslator() {
+  let pegaElemento = parseInt(document.getElementById("time").value) + 1;
+  contador = pegaElemento;
+  setInterval(myFunction, 1000);
+}
+
+function myFunction() {
+  if (contador > 0) {
+    contador -= 1;
+    const element = document.getElementById("retCronometro");
+    element.innerText = "faltam " + contador + " segundos";
+
+    alpha -= 0.15;
+    const root = document.getElementById("container");
+    root.style.backgroundColor = `rgb(${red},${green},${blue},${alpha})`;
+
+    const backDivOne = document.getElementById("divTwo");
+    backDivOne.style.background = "url('https://thumbs.gfycat.com/SelfreliantClutteredBlackpanther-max-1mb.gif')";
+    document.getElementById("divOne").style.display = "none";
+     
+      
+      if(contador == 0){
+      document.getElementById("divTwo").remove();
+      document.getElementById("retCronometro").remove();
+      
     
+      const backDivTwo = document.getElementById("divThree");
+      backDivTwo.style.background = "url('https://thumbs.gfycat.com/DifferentDimpledHamster-max-1mb.gif')";
+
+      var buttonReset = document.getElementById('reset');
+      buttonReset.insertAdjacentHTML('afterend', '<div id="reset"> <input type="button" value="Resetar" onClick="window.location.reload()"></div>');
+      }
+  }
   
+}
